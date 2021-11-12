3af77232e240135bd3765547a92b9de9e2978b71
elizaveta.mazurenko@pwc.ru <elizaveta.mazurenko@pwc.ru>
2021-11-12 10:34:15 +0000

    lifetime_value and fct_orders with right sum (1672)

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -6,23 +6,15 @@ orders as (
     select * from {{ ref('stg_orders')}}
 ),
 
-lifetime_value as (
-    select sum(amount) as lifetime_value, customer_id
-    from {{ref('fct_orders')}}
-    group by customer_id
-),
-
-customer_orders as (
-
-    select
+customer_orders as (select
         customer_id,
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
-    from orders
-
+    from {{ref('fct_orders')}}
     group by 1
 
 ),
@@ -37,13 +29,11 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(lifetime_value, 0) as lifetime_value
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
-    left join lifetime_value using (customer_id)
-
 )
 
 select * from final
--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,6 +1,18 @@
+with success_payments as ( 
+    select 
+        order_id, 
+        payment_id, 
+        amount,
+        status
+    from {{ref('stg_payments')}}
+    where status = 'success'
+)
+
+
 select
     order_id, 
     customer_id, 
-    amount 
-from {{ref('stg_payments')}}
+    amount,
+    order_date 
+from success_payments
 left join {{ref('stg_orders')}} using (order_id)
